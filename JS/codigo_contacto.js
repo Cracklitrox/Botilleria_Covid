7770e51b66f6b5fd27713a4fe2d8d84b17825abe
Christian_Cardenas <ch.cardenasi@duocuc.cl>
2023-05-03 15:53:13 -0400

    avanze a 2 semanas de la prueba final, se agregaron 2 archivos JS para validaciones y se hicieron cambios en la pagina HTML de Contacto

--- JS/codigo_contacto.js
+++ JS/codigo_contacto.js
@@ -1,28 +1,41 @@
-// Espera para que todos los elementos de la página se carguen para continuar el script
-if(document.readyState =='loading'){
-  document.addEventListener('DOMContentLoaded',ready)
-}else{
-  ready();
-}
+$("#contact-form").validate({
+  rules:{
+    name: {
+      required: true,
+      minlenght: 40,
+      maxlength: 80
+    }
+  },
+  messages: {
+    nombre: {
+      required: "Por favor, ingrese su nombre completo",
+      minlenght: "El nombre completo debe tener al menos 40 caracteres",
+      maxlength: "El nombre completo no puede tener más de 40 caracteres"
+    }
+  }
+})
 
-function ready(){
-  var form = document.getElementById("contact-form");
+// Deshabilitar el botón de envío al cargar la página, para una mejor funcion
+$("#submit").prop("disabled", true);
 
-  // Agregar un evento "submit" al formulario
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevenir el comportamiento por defecto del formulario (submit y recargar la página)
-  
-    // Obtencion de los valores de los campos del formulario
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var city = document.getElementById("city").value;
-    var message = document.getElementById("message").value;
-  
-    // Envia un mensaje para demostrar el correcto funcionamiento
-    alert("Tu mensaje se ha enviado correctamente");
-  
-    // Resetea los valores del formulario
-    form.reset();
-  });
-}
+// Funcion para detectar cuando se ingresa información veridica a los campos
+$("input, textarea").on("keyup", function() {
+  // Verificar si se han completado todos los campos requeridos
+  if ($("#contact-form").valid()) {
+      // Habilitar el botón de envío
+      $("#submit").prop("disabled", false);
+  } else {
+      // Deshabilitar el botón de envío
+      $("#submit").prop("disabled", true);
+  }
+});
+
+
+// Funcion que retornara cuando el boton para enviar los datos sea valido
+$("#submit").click(function(){
+  if($("#contact-form").valid() == false ){
+    return;
+  }
+  let nombre = $("#name").val()
+  alert("Datos enviados correctamente.");
+})
